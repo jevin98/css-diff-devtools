ef91561595c31a92d3e7fb332d6d1451e57b10fa
Jevin <jevin98@163.com>
2026-05-14 17:02:54 +0800

    refactor: use WXT i18n

--- entrypoints/devtools-panel/main.ts
+++ entrypoints/devtools-panel/main.ts
@@ -1,13 +1,6 @@
 import { createApp } from 'vue'
-import { createI18n } from 'vue-i18n'
 import App from './devtools-panel.vue'
 
-import messages from './lang'
 import 'element-plus/dist/index.css'
 
-const i18n = createI18n({
-  locale: 'en',
-  messages,
-})
-
-createApp(App).use(i18n).mount('#app')
+createApp(App).mount('#app')
